cda08918cd440e7d6f045109715435a10008dd3c
권태혁 <warsfac@gmail.com>
2026-06-15 09:56:08 +0900

    feat: 전체 카테고리(상품 리스트) 페이지 및 필터 UI 구현

--- frontend/web/src/router/index.js
+++ frontend/web/src/router/index.js
@@ -18,6 +18,11 @@ const router = createRouter({
       path: '/signup',
       name: 'signup',
       component: () => import('../views/SignupView.vue'),
+    },
+    {
+      path: '/category',
+      name: 'category',
+      component: () => import('../views/CategoryView.vue'),
     }
   ],
 })
